81c9052a87f7cba3909bf55b38350632489c7937
SpecDI <v.herghelegiu@warwick.ac.uk>
2020-03-10 20:44:53 +0000

    Deprecated previous augmentation file. Created new augg script that generates additional tubes. Still need to fix the gaussian noise function

--- data_augmentation.py
+++ data_augmentation.py
@@ -3,94 +3,123 @@ import os
 from glob import glob
 from PIL import Image
 from matplotlib import pyplot as plt
+import matplotlib
 from pathlib import Path
 import sys
 import math
+import shutil
+
+from skimage.util import random_noise
 
 from keras.preprocessing.image import img_to_array, ImageDataGenerator
 
 import argparse
 
-augmentation_prefix = 'augg'
+def tensor_random_noise(im):
+    return random_noise(im, mode='gaussian')
 
-def parse_args():
-  """ Parse command line arguments.
-  """
-  parser = argparse.ArgumentParser(description="Data Augmentation")
-  parser.add_argument(
-      "--mode", help="Mode = clean will result in deleting all augmented tubes. Mode = augmentation will result in augmentation. No argument will print stats"
-  )
-  parser.add_argument(
-      "--path", help="Path to action-tubes folder to be augmented",
-      required = True
-  )
-  return parser.parse_args()
-
-def clean(path_tubes):
-  print('Cleaning...')
-
-  for im_path in list(Path(".").rglob("*.jpg")):
-    im_name = str(im_path)
-    if augmentation_prefix in im_name:
-      os.remove(im_name)
-    
-
-def transform_image(img, tube_path, img_factor):
-  datagen = ImageDataGenerator( 
-        rotation_range = 40, 
+datagen = ImageDataGenerator( 
+        rotation_range = 15, 
         shear_range = 0.2, 
         zoom_range = 0.2, 
-        horizontal_flip = True, 
-        brightness_range = (0.5, 1.5))
+        brightness_range = (0.5, 1.5),
+        preprocessing_function = tensor_random_noise)
 
-  im = img_to_array(img)
-  im = im.reshape((1, ) + im.shape)
+def parse_args():
+    """ Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser(description="Data Augmentation")
+    parser.add_argument(
+        "--mode", help="Mode = clean will result in deleting all augmented tubes. Mode = augmentation will result in augmentation. No argument will print stats"
+    )
+    parser.add_argument(
+        "--path", help="Path to action-tubes folder to be augmented",
+        required = True
+    )
+    return parser.parse_args()
 
-  i = 0
-  for batch in datagen.flow(im, batch_size = 1, save_to_dir = tube_path, save_prefix = augmentation_prefix, save_format = 'jpg'):
-    i += 1
-    if i > img_factor:
-      break
+def clean_class(action_path, prefix):
+    print('Cleaning: ', os.path.basename(os.path.normpath(action_path)))
+    for tube in glob(action_path + '*/'):
+        if prefix in tube:
+            shutil.rmtree(tube)
 
-def augment_class(class_path, img_factor):
-  print(f"Augmenting: {class_path} with factor {img_factor}")
+def augment_class(action_path, scaling_factor, prefix):
+    print(f"Augmenting {os.path.basename(os.path.normpath(action_path))} with factor {scaling_factor}")
 
-  for tube_path in glob(class_path + '*/'):
-    for im_path in glob(tube_path + '*.jpg'):
-      if augmentation_prefix not in os.path.basename(os.path.normpath(im_path)):
-        img = np.asarray(Image.open(im_path))
-        transform_image(img, tube_path, img_factor)
-      
+    for tube in glob(action_path + '*/'):
+        if not prefix in tube:
+            # Generate target augmentated tube
+            generated_actionTubes = []
+            for i in range(scaling_factor):
+                target_dirName = action_path + prefix + str(i) + '_' + os.path.basename(os.path.normpath(tube))
+                generated_actionTubes.append(target_dirName)
+                os.mkdir(target_dirName)
 
-def main(mode, path_tubes):  
-  # Determine average tubes per class
-  tube_counts = []
-  for action_class in glob(path_tubes + '*/'):
-    tube_counts.append(sum([len(files) for r, d, files in os.walk(action_class)]))
+            # Populate tubes by transforming
+            image_list = glob(tube + '*.jpg')
+            for im_file in sorted(image_list):
+                transform_image(im_file, generated_actionTubes)
+                
+def transform_image(im_file, target_names):
+    im = img_to_array(Image.open(im_file))
+    im /= 255.0
+    
+    plt.imshow(im)
+    plt.show()
+    
+    original_shape = im.shape
+    im = im.reshape((1, ) + im.shape)
 
-  avg_tube_size = np.mean(tube_counts).astype(int)
+    i = 0
+    for batch in datagen.flow(im, batch_size = 1):
+        im = batch.reshape(original_shape).astype('uint8')
 
-  i = 0
-  for action_class in glob(path_tubes + '*/'):
-    img_factor = math.ceil((avg_tube_size - tube_counts[i]) / tube_counts[i])
+        plt.imshow(im)
+        plt.show()
 
-    print(f"{os.path.basename(os.path.normpath(action_class))}: {tube_counts[i]} -> {img_factor}")
-    i += 1
+        im_name = target_names[i] + '/' + os.path.basename(os.path.normpath(im_file))
+        matplotlib.image.imsave(im_name, im)
 
-  print(f"\nDiscovered images: {np.sum(tube_counts)}")
-  print(f"Average tube size: {avg_tube_size}")
+        i += 1
+        if i == len(target_names):
+            break
 
-  if mode == 'clean':
-    clean(path_tubes)
-  elif mode == 'augmentation':
-    # Augment classes with less than average
+def main(mode, path_tubes):
+    # Define augmentation prefix
+    prefix = 'augg_'
+
+    tube_map = {}
+    maxTube_count = 0
+    # Iterate over all action classes
     for action_class in glob(path_tubes + '*/'):
-      cnt = sum([len(files) for r, d, files in os.walk(action_class)])
+        # Get the number of tubes in a given class
+        current_count = len([x[0] for x in os.walk(action_class)]) - 1 # don't count . dir
+        
+        # Insert tube count in map
+        tube_map[action_class] = current_count
+        
+        # Update max count
+        if current_count > maxTube_count:
+            maxTube_count = current_count
+    
+    # Update count to scaling factor
+    for action_key in tube_map:
+        tube_map[action_key] = math.ceil(maxTube_count / tube_map[action_key])
 
-      if(cnt < avg_tube_size and os.path.basename(os.path.normpath(action_class)) != 'Unknown'):
-        img_factor = math.ceil((avg_tube_size - cnt) / cnt)
-        augment_class(action_class, img_factor)
+    # Print out identified action classes and factors
+    for action_key in tube_map:
+        print(f"{os.path.basename(os.path.normpath(action_key))} -> {tube_map[action_key]}")
 
+    # Augment each action dir based on map data
+    for action_key in tube_map:
+        if mode == 'augmentation':
+            augment_class(action_key, tube_map[action_key], prefix)
+        elif mode == 'clean':
+            clean_class(action_key, prefix)
+    
+    # print('Max action tube count: ', maxTube_count)
+    # print(tube_map)
 
 if __name__ == '__main__':
     # Parse user provided arguments
