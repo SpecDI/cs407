b40b478fa632a082eb16e65b0580026260f80edc
SpecDI <vlad@herghelegiu.com>
2020-04-07 18:45:38 +0300

    Version 1 of trackin eval. Normalization and more channels.

--- object_tracking_eval.py
+++ object_tracking_eval.py
@@ -1,3 +1,5 @@
+from operator import truediv, add
+
 from glob import glob
 import argparse
 from PIL import Image
@@ -12,6 +14,11 @@ import cv2
 
 from matplotlib import pyplot as plt
 
+
+# Implemente KS with channels
+# Try a local descriptor e.g., SIFT and SURF
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Similarity measure")
     parser.add_argument(
@@ -25,12 +32,15 @@ def compute_similarity(im1, im2, metric):
     # im1_hist, im1_edges = np.histogram(im1.ravel(), 64)
     # im2_hist, im2_edges = np.histogram(im2.ravel(), 64)
     
-    hist1 = cv2.calcHist([im1],[0],None,[256],[0,256])
-    hist2 = cv2.calcHist([im2],[0],None,[256],[0,256])
-
-    diff = cv2.compareHist(hist1, hist2, metric)
+    scores = []
     
-    return diff
+    for i in range(3):
+        hist1 = cv2.normalize(cv2.calcHist([im1],[i],None,[256],[0,256]), None, 0, 1, cv2.NORM_MINMAX)
+        hist2 = cv2.normalize(cv2.calcHist([im2],[i],None,[256],[0,256]), None, 0, 1, cv2.NORM_MINMAX)
+        
+        scores.append(cv2.compareHist(hist1, hist2, metric))
+    
+    return scores
 
 def eval_tube(tube_path):
     print(f'Processing {tube_path}')
@@ -47,30 +57,48 @@ def eval_tube(tube_path):
     
     # Iterate over all frames -1
     # Compute similarity
-    running_sum_chi = 0
-    running_sum_bhat = 0
-    running_sum_inter = 0
+    running_sum_chis = [0, 0, 0]
+    running_sum_bhats = [0, 0, 0]
+    running_sum_inters = [0, 0, 0]
     
     for i in range(len(tube_frames) - 1):
         im1 = tube_frames[i]
         im2 = tube_frames[i+1]
         
         # Compute difference
-        chi_score = compute_similarity(im1, im2, cv2.HISTCMP_CHISQR)
-        bhat_score = compute_similarity(im1, im2, cv2.HISTCMP_BHATTACHARYYA)
-        inter_score = compute_similarity(im1, im2, cv2.HISTCMP_INTERSECT)
+        chi_scores = compute_similarity(im1, im2, cv2.HISTCMP_CHISQR)
+        bhat_scores = compute_similarity(im1, im2, cv2.HISTCMP_BHATTACHARYYA)
+        inter_scores = compute_similarity(im1, im2, cv2.HISTCMP_INTERSECT)
         
-        chi_factor = chi_score / (running_sum_chi / i) if i != 0 else 1
-        bhat_factor = bhat_score / (running_sum_bhat / i) if i != 0 else 1
-        inter_factor = inter_score / (running_sum_inter / i) if i != 0 else 1
+        #chi_factors = chi_scores / (running_sum_chis / i) if i != 0 else 1
+        if i > 0:
+            averages = list(map(truediv, running_sum_chis, [i ,i, i]))
+            chi_factors = list(map(truediv, chi_scores, averages))
+            
+            averages = list(map(truediv, running_sum_bhats, [i ,i, i]))
+            bhat_factors = list(map(truediv, bhat_scores, averages))
+            
+            averages = list(map(truediv, running_sum_inters, [i ,i, i]))
+            inter_factors = list(map(truediv, inter_scores, averages))
+        else:
+            chi_factors = [1, 1, 1]
+            bhat_factors = [1, 1, 1]
+            inter_factors = [1, 1, 1]
         
-        running_sum_chi += chi_score
-        running_sum_bhat += bhat_score
-        running_sum_inter += inter_score
+        # bhat_factors = bhat_scores / (running_sum_bhats / i) if i != 0 else 1
+        # inter_factors = inter_scores / (running_sum_inters / i) if i != 0 else 1
         
-        print('CHI: ', round(chi_score, 2), '|', round(chi_factor, 2))
-        print('BHAT: ', round(bhat_score, 2), '|', round(bhat_factor, 2))
-        print('INTER: ', round(inter_score, 2), '|', round(inter_factor, 2))
+        running_sum_chis = list(map(add, chi_scores, running_sum_chis))
+        running_sum_bhats = list(map(add, bhat_scores, running_sum_bhats))
+        running_sum_inters = list(map(add, inter_scores, running_sum_inters))
+        
+        print('chi FACTORS: ', chi_factors, np.mean(chi_factors))
+        print('bhat FACTORS: ', bhat_factors, np.mean(bhat_factors))
+        print('inter FACTORS: ', inter_factors, np.mean(inter_factors))
+        
+        #print('CHI: ', round(chi_score, 2), '|', round(chi_factor, 2))
+        # print('BHAT: ', round(bhat_score, 2), '|', round(bhat_factor, 2))
+        # print('INTER: ', round(inter_score, 2), '|', round(inter_factor, 2))
         print('')
         
         # Display them
@@ -97,7 +125,7 @@ def eval_tube(tube_path):
 def main(video_name):
     video_path = f'./results/object_tracking/{video_name}/**/'
     
-    eval_tube('./results/object_tracking/webServer_inputVideo/663/')
+    eval_tube('./results/object_tracking/webServer_inputVideo/140/')
     # for tube_path in sorted(glob(video_path)):
     #     eval_tube(tube_path)
 
