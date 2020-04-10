9b53e6a90ca9e9d4fcd6771435a3bbafc6adf9d1
SpecDI <vlad@herghelegiu.com>
2020-04-10 16:19:19 +0300

    Version 2.0 done

--- object_tracking_eval.py
+++ object_tracking_eval.py
@@ -5,6 +5,10 @@ import argparse
 from PIL import Image
 import os
 import numpy as np
+import pandas as pd
+import sys
+
+from tqdm import tqdm
 
 from scipy.stats import ks_2samp
 from scipy.stats import norm
@@ -25,25 +29,25 @@ def parse_args():
         '--video_name', help = 'Name of the video to be evaluated in results dir',
         required = True
     )
+    parser.add_argument(
+        '--tube_name', help = 'Tube identifier for tube inspection',
+        default = None
+    )
     return parser.parse_args()
 
 def compute_similarity(im1, im2, metric):
     # Compute the image histograms
-    # im1_hist, im1_edges = np.histogram(im1.ravel(), 64)
-    # im2_hist, im2_edges = np.histogram(im2.ravel(), 64)
-    
     scores = []
     
     for i in range(3):
-        hist1 = cv2.normalize(cv2.calcHist([im1],[i],None,[256],[0,256]), None, 0, 1, cv2.NORM_MINMAX)
-        hist2 = cv2.normalize(cv2.calcHist([im2],[i],None,[256],[0,256]), None, 0, 1, cv2.NORM_MINMAX)
+        hist1 = cv2.calcHist([im1],[i],None,[256],[0,256])
+        hist2 = cv2.calcHist([im2],[i],None,[256],[0,256])
         
         scores.append(cv2.compareHist(hist1, hist2, metric))
     
-    return scores
+    return np.mean(scores)
 
-def eval_tube(tube_path):
-    print(f'Processing {tube_path}')
+def eval_tube(tube_path, test_mode = False):
     tube_frames = []
     
     frame_nums = sorted([int(os.path.splitext(os.path.basename(file_path))[0]) for file_path in glob(tube_path + '*.jpg')])
@@ -57,80 +61,85 @@ def eval_tube(tube_path):
     
     # Iterate over all frames -1
     # Compute similarity
-    running_sum_chis = [0, 0, 0]
-    running_sum_bhats = [0, 0, 0]
-    running_sum_inters = [0, 0, 0]
+    diff_list = []
     
     for i in range(len(tube_frames) - 1):
         im1 = tube_frames[i]
         im2 = tube_frames[i+1]
         
         # Compute difference
-        chi_scores = compute_similarity(im1, im2, cv2.HISTCMP_CHISQR)
-        bhat_scores = compute_similarity(im1, im2, cv2.HISTCMP_BHATTACHARYYA)
-        inter_scores = compute_similarity(im1, im2, cv2.HISTCMP_INTERSECT)
-        
-        #chi_factors = chi_scores / (running_sum_chis / i) if i != 0 else 1
-        if i > 0:
-            averages = list(map(truediv, running_sum_chis, [i ,i, i]))
-            chi_factors = list(map(truediv, chi_scores, averages))
-            
-            averages = list(map(truediv, running_sum_bhats, [i ,i, i]))
-            bhat_factors = list(map(truediv, bhat_scores, averages))
-            
-            averages = list(map(truediv, running_sum_inters, [i ,i, i]))
-            inter_factors = list(map(truediv, inter_scores, averages))
-        else:
-            chi_factors = [1, 1, 1]
-            bhat_factors = [1, 1, 1]
-            inter_factors = [1, 1, 1]
-        
-        # bhat_factors = bhat_scores / (running_sum_bhats / i) if i != 0 else 1
-        # inter_factors = inter_scores / (running_sum_inters / i) if i != 0 else 1
-        
-        running_sum_chis = list(map(add, chi_scores, running_sum_chis))
-        running_sum_bhats = list(map(add, bhat_scores, running_sum_bhats))
-        running_sum_inters = list(map(add, inter_scores, running_sum_inters))
-        
-        print('chi FACTORS: ', chi_factors, np.mean(chi_factors))
-        print('bhat FACTORS: ', bhat_factors, np.mean(bhat_factors))
-        print('inter FACTORS: ', inter_factors, np.mean(inter_factors))
-        
-        #print('CHI: ', round(chi_score, 2), '|', round(chi_factor, 2))
-        # print('BHAT: ', round(bhat_score, 2), '|', round(bhat_factor, 2))
-        # print('INTER: ', round(inter_score, 2), '|', round(inter_factor, 2))
-        print('')
+        diff_list.append(compute_similarity(im1, im2, cv2.HISTCMP_BHATTACHARYYA))
         
         # Display them
-        fig = plt.figure(figsize = (10, 10))
-        plt.subplot(2, 2, 1)
-        plt.imshow(im1)
-        plt.title(frame_nums[i])
-        plt.axis('off')
-        
-        plt.subplot(2, 2, 2)
-        plt.imshow(im2)
-        plt.title(frame_nums[i+1])
-        plt.axis('off')
-        
-        plt.subplot(2, 2, 3)
-        plt.hist(im1.ravel(),256,[0,256])
-        
-        plt.subplot(2, 2, 4)
-        plt.hist(im2.ravel(),256,[0,256])
-        
-        plt.show()
+        if test_mode:
+            fig = plt.figure(figsize = (10, 10))
+            plt.subplot(2, 2, 1)
+            plt.imshow(im1)
+            plt.title(f"Comparing: {frame_nums[i]} and {frame_nums[i+1]}")
+            plt.axis('off')
+            
+            plt.subplot(2, 2, 2)
+            plt.imshow(im2)
+            plt.title(diff_list[-1])
+            plt.axis('off')
+            
+            plt.subplot(2, 2, 3)
+            plt.hist(im1.ravel(),256,[0,256])
+            
+            plt.subplot(2, 2, 4)
+            plt.hist(im2.ravel(),256,[0,256])
+            
+            plt.show()
+    return diff_list
         
 
-def main(video_name):
-    video_path = f'./results/object_tracking/{video_name}/**/'
+def main(video_name, tube_name):
+    video_path = f'./results/object_tracking/{video_name}/'
     
-    eval_tube('./results/object_tracking/webServer_inputVideo/140/')
-    # for tube_path in sorted(glob(video_path)):
-    #     eval_tube(tube_path)
-
+    if(tube_name is not None):
+        print(eval_tube(video_path + tube_name + '/', True))
+        sys.exit()
+        
+    video_diff_list = {}
+    
+    global_diffs = []
+    
+    tube_nums = sorted([int(os.path.basename(os.path.normpath(path))) for path in glob(video_path + '**/')])
+    for tube_num in tqdm(tube_nums):
+        tube_path = video_path + str(tube_num) + '/'
+        
+        video_diff_list[tube_num] = eval_tube(tube_path)
+        global_diffs.extend(video_diff_list[tube_num])
+    
+    df = pd.DataFrame(columns = ['tube_path', 'tube_length', 'tube_mean', 'tube_min', 'tube_max', 'tube_std'])
+    for key in video_diff_list:
+        tube_diff_list = video_diff_list[key]
+        
+        # Frame count
+        tube_length = len(tube_diff_list) + 1 
+        
+        # Mean
+        tube_mean = np.mean(tube_diff_list)
+        
+        # Min/Max
+        tube_min = np.min(tube_diff_list)
+        tube_max = np.max(tube_diff_list)
+        
+        # Standard deviation
+        tube_std = np.std(tube_diff_list)
+        
+        df.loc[len(df)] = [key, tube_length, tube_mean, tube_min, tube_max, tube_std]
+        
+    print(df)
+    print(f"\nAverage tube mean: {df['tube_mean'].mean()}")
+    print(f"Average tube std: {df['tube_std'].std()}\n")
+    
+    print('=====GLOBAL METRICS=====')
+    print(f"Global average: {np.mean(global_diffs)}")
+    print(f"Global variance: {np.var(global_diffs)}")
+    print(f"Global std: {np.std(global_diffs)}")
 
 if __name__ == '__main__':
   # Parse user provided arguments
   args = parse_args()
-  main(args.video_name)
+  main(args.video_name, args.tube_name)
