7ef9ad59f1c2d59dd94748da0bb7d3f858db5c9b
u1607854 <43875552+u1607854@users.noreply.github.com>
2020-05-06 10:00:42 +0100

    Updated Redis credentials.

--- gui/webapp/webapp/settings.py
+++ gui/webapp/webapp/settings.py
@@ -113,11 +113,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = '/media/'
 
-#CELERY_BROKER_URL = 'redis://h:pb9870e090ecfc3675d484c94eee7ec8589035888c3b01d204791a04c8cd25f80@ec2-52-208-3-240.eu-west-1.compute.amazonaws.com:7089'
-CELERY_BROKER_URL = 'redis://h:pb9870e090ecfc3675d484c94eee7ec8589035888c3b01d204791a04c8cd25f80@ec2-34-246-4-198.eu-west-1.compute.amazonaws.com:24549'
+# CELERY_BROKER_URL = 'redis://h:pb9870e090ecfc3675d484c94eee7ec8589035888c3b01d204791a04c8cd25f80@ec2-52-208-3-240.eu-west-1.compute.amazonaws.com:7089'
+# CELERY_BROKER_URL = 'redis://h:pb9870e090ecfc3675d484c94eee7ec8589035888c3b01d204791a04c8cd25f80@ec2-34-246-4-198.eu-west-1.compute.amazonaws.com:24549'
+CELERY_BROKER_URL = 'redis://h:pb9870e090ecfc3675d484c94eee7ec8589035888c3b01d204791a04c8cd25f80@ec2-52-30-249-18.eu-west-1.compute.amazonaws.com:17159'
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 
 # CELERY_RESULT_BACKEND = "redis://h:pb9870e090ecfc3675d484c94eee7ec8589035888c3b01d204791a04c8cd25f80@ec2-52-208-3-240.eu-west-1.compute.amazonaws.com:7089"
-CELERY_RESULT_BACKEND = 'redis://h:pb9870e090ecfc3675d484c94eee7ec8589035888c3b01d204791a04c8cd25f80@ec2-34-246-4-198.eu-west-1.compute.amazonaws.com:24549'
+# CELERY_RESULT_BACKEND = 'redis://h:pb9870e090ecfc3675d484c94eee7ec8589035888c3b01d204791a04c8cd25f80@ec2-34-246-4-198.eu-west-1.compute.amazonaws.com:24549'
+CELERY_RESULT_BACKEND = 'redis://h:pb9870e090ecfc3675d484c94eee7ec8589035888c3b01d204791a04c8cd25f80@ec2-52-30-249-18.eu-west-1.compute.amazonaws.com:17159'
